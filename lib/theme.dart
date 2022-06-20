00114c3789e2ce225d66377fcbcca55f21e37fc4
Muhammad Aswan <muhaswan190897@gmail.com>
2022-06-20 14:24:43 +0800

    custom main and add theme

---
+++ lib/theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = Color(0xff6C5ECF);
+Color secondaryColor = Color(0xff38ABBE);
+Color alertColor = Color(0xffED6363);
+Color priceColor = Color(0xff2C96F1);
+Color backgroundColor1 = Color(0xff1F1D28);
+Color backgroundColor2 = Color(0xff2B2937);
+Color backgroundColor3 = Color(0xff242231);
+Color primaryTextColor = Color(0xffF1F0F2);
+Color secondaryTextColor = Color(0xff999999);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: primaryTextColor,
+);
+
+TextStyle secondaryTextStyle = GoogleFonts.poppins(
+  color: secondaryTextColor,
+);
+
+TextStyle priceTextStyle = GoogleFonts.poppins(
+  color: priceColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w100;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
