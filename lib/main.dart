ffca44177e75b20db7b22f7e34b747c1643bbe2c
Muhammad Aswan <muhaswan190897@gmail.com>
2022-08-22 09:07:57 +0800

    slicing splash and sign in page

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,21 @@
 import 'package:flutter/material.dart';
-import 'theme.dart';
 
-void main() => runApp(MyApp());
+import 'pages/sign_in_page.dart';
+import 'pages/splash_page.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Halo semuanya!',
-          style: priceTextStyle.copyWith(
-            fontSize: 30,
-          ),
-        ),
-      ),
+      routes: {
+        '/': (context) => const SplashPage(),
+        '/sign-in': (context) => const SignInPage(),
+      },
     );
   }
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Color primaryColor = Color(0xff6C5ECF);
-Color secondaryColor = Color(0xff38ABBE);
-Color alertColor = Color(0xffED6363);
-Color priceColor = Color(0xff2C96F1);
-Color backgroundColor1 = Color(0xff1F1D28);
-Color backgroundColor2 = Color(0xff2B2937);
-Color backgroundColor3 = Color(0xff242231);
-Color primaryTextColor = Color(0xffF1F0F2);
-Color secondaryTextColor = Color(0xff999999);
+double defaultMargin = 30.0;
+
+Color primaryColor = const Color(0xff6C5ECF);
+Color secondaryColor = const Color(0xff38ABBE);
+Color alertColor = const Color(0xffED6363);
+Color priceColor = const Color(0xff2C96F1);
+Color backgroundColor1 = const Color(0xff1F1D28);
+Color backgroundColor2 = const Color(0xff2B2937);
+Color backgroundColor3 = const Color(0xff242231);
+Color primaryTextColor = const Color(0xffF1F0F2);
+Color secondaryTextColor = const Color(0xff999999);
+Color subtitleColor = const Color(0xff504F5E);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: primaryTextColor,
@@ -19,8 +22,16 @@ TextStyle secondaryTextStyle = GoogleFonts.poppins(
   color: secondaryTextColor,
 );
 
+TextStyle subtitleTextStyle = GoogleFonts.poppins(
+  color: subtitleColor,
+);
+
 TextStyle priceTextStyle = GoogleFonts.poppins(
   color: priceColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: primaryColor,
 );
 
 FontWeight light = FontWeight.w300;
